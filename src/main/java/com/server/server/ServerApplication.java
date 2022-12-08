8f182e070f0f69112b96195bcf0c22fd9ee18129
Johnathan Zhao <johnathan.zhao@stonybrook.edu>
2022-12-08 15:31:02 -0500

    Can read CSV File. Need to fix endpoints.

--- src/main/java/com/server/server/ServerApplication.java
+++ src/main/java/com/server/server/ServerApplication.java
@@ -1,17 +1,34 @@
 package com.server.server;
 
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
 import com.server.server.config.DBConfigProperties;
 import com.server.server.config.JPAConfigProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.util.ResourceUtils;
+
+import java.io.FileReader;
+import java.util.List;
 
 @SpringBootApplication
 @EnableConfigurationProperties({DBConfigProperties.class, JPAConfigProperties.class})
 public class ServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
-
+		try{
+			FileReader fileReader=new FileReader(ResourceUtils.getFile("classpath:data/Ohio/Ohio_Enacted_Demographics.csv"));
+			CSVReader csvReader=new CSVReaderBuilder(fileReader).withSkipLines(1).build();
+			List<String[]> allData=csvReader.readAll();
+			for(String[] row:allData){
+				for(String cell:row){
+					System.out.println(cell);
+				}
+			}
+		}catch (Exception e){
+			System.out.println(e.getMessage());
+		}
 	}
 
 }
